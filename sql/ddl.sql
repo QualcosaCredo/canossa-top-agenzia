8c00b99243186fbd179a0740dc5db067de93a3c7
zyro <manav.kumar@galileiostiglia.edu.it>
2025-02-24 11:47:16 +0100

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,57 +1,63 @@
-CREATE TABLE VENDITE (
-    id integer unsigned auto_increment,
-    prezzovendita varchar(20) not null,
-    datavendita varchar(20) not null,
-    prezzo varchar(20) not null,
-    provvigione varchar(20) not null,
-    foreign key (codiceAcquirente),
-    foreign key (codiceImmobile),
-    primary key(codiceVendita)
-)ENGINE=InnoDB;
-
-CREATE TABLE IMMOBILI (
-    id integer unsigned auto_increment,
-    tipologia varchar(20) not null,
-    prezzoRichiesto varchar(20) not null,
-    superficie varchar(20) not null,
-    quartiere varchar(20) not null,
-    indirizzo varchar(20) not null,
-    dataDisponibilita varchar(20) not null,
-    dataInserimento varchar(20) not null,
-    stato varchar(20) not null,
-    foreign key (codiceVenditore),
-    foreign key (codiceProprietario),
-    primary key(codiceImmobile)
-)ENGINE=InnoDB;
-
-CREATE TABLE VISITE (
-    id integer unsigned auto_increment,
-    datavisita varchar(20) not null,
-    foreign key (codiceAcquirente),
-    foreign key (codiceImmobile),
-    primary key(codiceVisita)
-)ENGINE=InnoDB;
-
 CREATE TABLE VENDITORE (
-    id integer unsigned auto_increment,
-    nome varchar(20) not null,
-    cognome varchar(20) not null,
-    acquirente varchar(20) not null,
-    primary key(codiceVenditore)
+    CodiceVenditore integer unsigned auto_increment,
+    nome varchar(20) DEFAULT '' not null,
+    cognome varchar(20) DEFAULT '' not null,
+    acquirente varchar(20) DEFAULT ''not null,
+    primary key(CodiceVenditore)
 )ENGINE=InnoDB;
 
 CREATE TABLE ACQUIRENTE (
-    id integer unsigned auto_increment,
-    nome varchar(20) not null,
-    cognome varchar(20) not null,
-    telefono varchar(20) not null,
-    primary key(codiceAcquirente)
+    CodiceAcquirente integer unsigned auto_increment,
+    nome varchar(20) DEFAULT ''not null,
+    cognome varchar(20) DEFAULT '' not null,
+    telefono varchar(20) DEFAULT '' not null,
+    primary key(CodiceAcquirente)
+)ENGINE=InnoDB;
+CREATE TABLE PROPRIETARIO (
+    codiceProprietario integer unsigned auto_increment,
+    nome varchar(20) DEFAULT '' not null,
+    cognome varchar(20) DEFAULT ''not null,
+    telefono varchar(20) DEFAULT '' not null,
+    primary key(codiceProprietario)
+)ENGINE=InnoDB;
+CREATE TABLE IMMOBILI (
+    CodiceImmobile integer unsigned auto_increment,
+    tipologia varchar(20) DEFAULT '' not null,
+    prezzorichiesto varchar(20) DEFAULT '' not null,
+    superficie varchar(20) DEFAULT '' not null,
+    quartiere varchar(20) DEFAULT '' not null,
+    indirizzo varchar(64) DEFAULT '' not null,
+    datadisponibilita varchar(20) DEFAULT '' not null,
+    datainserimento varchar(20) DEFAULT '' not null,
+    CodiceVenditore integer unsigned not null,
+    CodiceProprietario integer unsigned not null,
+    stato varchar(20)DEFAULT '' not null,
+    foreign key (CodiceVenditore)REFERENCES VENDITORE(CodiceVenditore),
+    foreign key (CodiceProprietario)REFERENCES PROPRIETARIO(CodiceProprietario),
+    primary key(CodiceImmobile)
 )ENGINE=InnoDB;
 
-CREATE TABLE PROPRIETARIO (
-    id integer unsigned auto_increment,
-    nome varchar(20) not null,
-    cognome varchar(20) not null,
-    telefono varchar(20) not null,
-    primary key(codiceProprietario)
+CREATE TABLE VENDITE (
+    CodiceVendita integer unsigned auto_increment,
+    prezzovendita varchar(20)DEFAULT '' not null,
+    datavendita varchar(20)DEFAULT '' not null,
+    prezzo varchar(20)DEFAULT '' not null,
+    provvigione varchar(20)DEFAULT '' not null,
+    CodiceAcquirente integer unsigned not null,
+    CodiceImmobile integer unsigned not null,
+    foreign key (CodiceAcquirente) REFERENCES acquirente(CodiceAcquirente),
+    foreign key (CodiceImmobile) REFERENCES immobili(CodiceImmobile),
+    primary key(CodiceVendita)
+)ENGINE=InnoDB;
+
+CREATE TABLE VISITE (
+    CodiceVisita integer unsigned auto_increment,
+    datavisita varchar(20)DEFAULT '' not null,
+    CodiceAcquirente integer unsigned not null,
+    CodiceImmobile integer unsigned not null,
+    CodiceVenditore integer unsigned not null,
+    foreign key (CodiceAcquirente)REFERENCES ACQUIRENTE(CodiceAcquirente),
+    foreign key (CodiceImmobile)REFERENCES IMMOBILI(CodiceImmobile),
+    foreign key (CodiceVenditore)REFERENCES VENDITORE(CodiceVenditore),
+    primary key(CodiceVisita)
 )ENGINE=InnoDB;
