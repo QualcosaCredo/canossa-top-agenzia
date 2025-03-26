b74515bea292215f60219df7975d57bc7fd5e186
QualcosaCredo <120122239+QualcosaCredo@users.noreply.github.com>
2025-03-26 20:37:55 +0100

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gestione Agenzia Immobiliare</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="../css/navbar.css" rel = "stylesheet">
+</head>
+<body>
+    <div class="navbar" id = "main_page_navbar">
+        <a href="index.php">🏠 Home</a>
+        <a href="immobili.php">📋 Immobili</a>
+        <a href="vendite.php">📝 Vendite</a>
+        <a href="contatti.php">📞 Contatti</a>
+        <a href="../include/logout.php">Logout</a>
+    </div>
+    <div class="container">
+        <h1>🏡 Gestione Agenzia Immobiliare</h1>
+        <a href="immobili.php" class="btn"><img src="https://cdn-icons-png.flaticon.com/128/684/684908.png" class="icon"> Visualizza Immobili</a>
+        <a href="vendite.php" class="btn"><img src="https://cdn-icons-png.flaticon.com/128/3062/3062634.png" class="icon"> Registra una Vendita</a>
+    </div>
+</body>
+</html>
